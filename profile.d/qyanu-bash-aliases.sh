b70a3c379f79cf78d9aac12053d77f87ee3514cb
Max-Julian Pogner <max-julian@pogner.at>
2020-07-26 01:17:31 +0200

    also eval dircolors

--- profile.d/qyanu-bash-aliases.sh
+++ profile.d/qyanu-bash-aliases.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 # some shortcuts
+test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 alias lsa="ls -alhi --color=auto"
 alias psa="ps -Alf"
 alias apgdef="apg -m22 -x22 -a1 -Mncl"
