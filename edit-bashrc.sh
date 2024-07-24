26afeb49c5375445567444ddb58208e5ffeb1a55
Max-Julian Pogner <max-julian@pogner.at>
2024-07-24 22:25:52 +0200

    edit-bashrc: preserve arbitrary placement of marked section

--- edit-bashrc.sh
+++ edit-bashrc.sh
@@ -9,14 +9,45 @@ if [[ ! -r "$FILETOEDIT" ]]; then
     echo "ERROR: cannot find file to edit: $FILETOEDIT" >&2
     exit 1
 fi
-}
 
-sed -r \
-  -e "/^## BEGIN: ${marker}/,/^## END: ${marker}/d" \
-  -i "$FILETOEDIT"
-(
-echo "## BEGIN: ${marker}"
-echo . /etc/profile.d/qyanu-bash-aliases.sh
-echo . /etc/profile.d/qyanu-bash-prompt.sh
-echo "## END: ${marker}"
-) | tee -a "$FILETOEDIT"
+
+begin_marker_rex="## BEGIN: ${marker}"
+end_marker_rex="## END: ${marker}"
+
+# ensure begin marker exists
+if ! grep -qPe "${begin_marker_rex}" "${FILETOEDIT}"; then
+    echo "## BEGIN: ${marker}" >> "${FILETOEDIT}"
+fi
+
+# ensure an end marker after the begin marker
+if ! sed -n -re "/${begin_marker_rex}/,/${end_marker_rex}/ p" \
+    "${FILETOEDIT}" \
+    | grep -qPe "${end_marker_rex}";
+then
+    sed -rf <(cat <<EOF
+/${begin_marker_rex}/ {
+    a ## END: ${marker}
+}
+EOF
+    ) \
+    -i "${FILETOEDIT}"
+fi
+
+# replace contents between markers with new contents
+sed -rf <(cat <<EOF
+/${begin_marker_rex}/,/${end_marker_rex}/ {
+    //!d
+    /${begin_marker_rex}/ {
+        p
+        e cat
+        d
+    }
+}
+EOF
+    ) \
+    -i "${FILETOEDIT}" \
+    < <(cat <<EOF
+. /etc/profile.d/qyanu-bash-aliases.sh
+. /etc/profile.d/qyanu-bash-prompt.sh
+EOF
+    )
